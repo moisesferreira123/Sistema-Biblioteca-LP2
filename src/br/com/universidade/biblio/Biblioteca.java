4b84448537135659fc3f57c8c94e7f16ae3fe79d
lucasaamorim <lucasapolonio@ufrn.edu.br>
2025-09-08 13:26:52 -0300

    Commit Inicial - Esqueleto da Atividade

---
+++ src/br/com/universidade/biblio/Biblioteca.java
@@ -0,0 +1,19 @@
+package br.com.universidade.biblio;
+
+import br.com.universidade.biblio.Usuario;
+import br.com.universidade.biblio.Livro;
+import br.com.universidade.biblio.Emprestimo;
+
+public class Biblioteca {
+    private Usuario[] usuarios;
+    private Livro[] livros;
+    private Emprestimo[] emprestimos;
+
+    public Emprestimo registrarEmprestimo(Usuario u, Livro l) {
+
+    }
+
+    public Emprestimo registrarDevolucao(Usuario u, Livro l) {
+        
+    }
+}
---
+++ src/br/com/universidade/biblio/Relatorio.java
@@ -0,0 +1,5 @@
+package br.com.universidade.biblio;
+
+public class Relatorio {
+    
+}
