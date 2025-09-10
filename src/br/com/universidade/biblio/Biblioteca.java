06cbe7cf4b2f7add4956fed498f0d8d2d03baa80
Moisés Ferreira <moisesaoaoao@gmail.com>
2025-09-10 18:47:07 -0300

    Registrar empréstimo desenvolvido (falta ajeitar aalgumas coisas)

--- src/br/com/universidade/biblio/Biblioteca.java
+++ src/br/com/universidade/biblio/Biblioteca.java
@@ -5,17 +5,57 @@ import br.com.universidade.biblio.Livro;
 import br.com.universidade.biblio.Emprestimo;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Biblioteca {
-    private ArrayList<Usuario> usuarios = new ArrayList<>();
-    private ArrayList<Livro> livros = new ArrayList<>();
-    private ArrayList<Emprestimo> emprestimos = new ArrayList<>();
+    private Usuario usuario = new Usuario();
+    private Livro livro = new Livro();
 
-    public Emprestimo registrarEmprestimo(Usuario u, Livro l) {
+    public Emprestimo registrarEmprestimo(Usuario u, Livro l, Date dataDeDevolucao) {
+        GerenciadorDeDados gd = new GerenciadorDeDados();
+        Emprestimo emprestimo = new Emprestimo(new Date(), dataDeDevolucao);
+        
+        // if(gd.consultarUsuarioBanco() == null) {
+        //     System.out.println("Usuário inexistente.");
+        //     return null;
+        // }
+        
+        // if(gd.consultarLivroBanco() == null) {
+        //     System.out.println("Livro inexistente");
+        //     return null;
+        // }
 
+        if(dataDeDevolucao.before(new Date())) {
+            System.out.println("Data de devolução inválida. Realize um empréstimo com a data após a data atual.");
+            return null;
+        }
+
+        if(!gd.registrarEmprestimoBanco(emprestimo)) {
+            System.out.println("Falha ao registrar empréstimo.");
+            return null;
+        }
+
+        u.adicionarEmprestimo(l);
+        return emprestimo;
     }
 
     public Emprestimo registrarDevolucao(Usuario u, Livro l) {
         
+    }
+
+    public boolean adicionarLivro() {
+
+    }
+
+    public ArrayList<Livro> listarLivros() {
+        
+    }
+
+    public boolean adicionarUsuario() {
+
+    }
+
+    public ArrayList<Usuario> listarUsuarios() {
+
     }
 }
